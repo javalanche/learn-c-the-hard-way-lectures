e0020092b11074f5060657287be8975bc3172a8a
Javier Fraga <javier.s.fraga@gmail.com>
2016-11-09 20:13:11 -0600

    adds up to ex12

---
+++ ex12/practice/ex12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int areas[] = { 10 , 12 , 13 , 14 , 20 };
+    char name[] = "zed";
+    char full_name[] = {
+        'Z' , 'e' , 'd',
+        ' ' , 'A' , '.' , ' ' ,
+        'S' , 'h' , 'a' , 'w' , '\0'
+    };
+
+    /*WARNINGS: On some systems you may have to change the*/
+    // %ld in this code to a %u since it will use unsigned ints
+
+    return 0;
+}
